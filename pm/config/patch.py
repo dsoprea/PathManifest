7927d0eb1091e698852aad2d8d3679898fbfd6ad
Dustin Oprea <myselfasunder@gmail.com>
2015-07-23 19:16:02 -0400

    We now allow dashes in the patch-names.

--- pm/config/patch.py
+++ pm/config/patch.py
@@ -3,4 +3,4 @@ PATCH_FILENAME_TEMPLATE = 'pm-patch-%(name)s.tar.bz2'
 PATCH_INFO_FILENAME_TEMPLATE = '.patch_info.%(name)s'
 
 DEFAULT_MANIFEST_FILENAME = '.manifest.csv.bz2'
-NAME_RX = r"^[a-zA-Z0-9_]+$"
+NAME_RX = r"^[a-zA-Z0-9_\-]+$"
